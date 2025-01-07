69fcb3e19784cb56fafa45230cde19fa00d3519c
Romma711 <andresitox01@gmail.com>
2025-01-06 22:25:23 -0300

    Really quick CORS fix

--- server/pkg/utils/utils.go
+++ server/pkg/utils/utils.go
@@ -11,4 +11,5 @@ func EnableCORS (w *http.ResponseWriter){
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 }
