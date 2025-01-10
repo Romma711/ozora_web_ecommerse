dc29518915e5f5625e6e5e6796965ef4b95ab8ce
Romma711 <andresitox01@gmail.com>
2025-01-10 15:20:51 -0300

    FINALLY CORS ENABLED i guess

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -25,26 +25,29 @@ func main() {
 
 	r := mux.NewRouter()
 
-	c := utils.EnableCORS()
-
-	muxCors := c.Handler(r)
+	
 	productStore := product.NewStoreDB(db)
 	filtersStore := filters.NewStore(db)
 	userStore := user.NewStore(db)
 	cartStore := cart.NewStore(db)
 	orderStore := order.NewStore(db)
-
+	
 	productHandler := product.NewHandler(productStore)
 	filtersHandler := filters.NewHandler(filtersStore)
 	userHandler := user.NewHandler(userStore)
 	cartHandler := cart.NewHandler(cartStore)
 	orderHandler := order.NewHandler(orderStore, productStore)
-
+	
+	
 	productHandler.GetProductRoutes(r)
 	filtersHandler.GetFiltersRoutes(r)
 	userHandler.GetUsersRoutes(r)
 	cartHandler.GetCartRoutes(r)
 	orderHandler.GetRoutes(r)
+	
+	c := utils.EnableCORS()
+	muxCors := c.Handler(r)
+
 	fmt.Println("Servidor iniciado en el puerto " + os.Getenv("DB_PORT"))
 	log.Fatal(http.ListenAndServe(os.Getenv("DB_HOST")+":"+os.Getenv("DB_PORT"), muxCors))
 }
